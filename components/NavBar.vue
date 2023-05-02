a77dcdd03fbec1522ba143746dd309226751cb53
haflettjm <hafletjm01@gmail.com>
2023-05-01 23:41:25 -0500

    just playing with navbar and getting styling semi done

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,3 +1,31 @@
 <template>
-    <h1>Jacob Haflett</h1>
+    <div class="NavBar">
+        <div>
+            <button>Nav</button>
+        </div>
+        <div>
+            <h1>Jacob Haflett</h1>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+    .NavBar{
+        position: relative;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        text-align: center;
+        height: 84px;
+    }
+    .NavBar div{
+        width: 30%;
+        height: 60%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        font-size: large;
+
+    }
+</style>
